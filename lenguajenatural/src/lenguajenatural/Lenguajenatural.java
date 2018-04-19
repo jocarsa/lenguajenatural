0f253e636beb2ee638447dc77dcc63df6192ab23
jocarsa <info@josevicentecarratala.com>
2018-04-19 20:10:27 +0200

    Programa en funcionamiento minimo

--- lenguajenatural/src/lenguajenatural/Lenguajenatural.java
+++ lenguajenatural/src/lenguajenatural/Lenguajenatural.java
@@ -49,16 +49,39 @@ public class Lenguajenatural extends JFrame {
         System.out.println("Has tocado el boton");
         
         try {
+            // Como Raul dice que igual hasta hay un java.sql ya dentro de Java, especifico la libreria que voy a usar
             Class.forName("com.mysql.jdbc.Driver") ;
+            // Creo una conexión con la base de datos introduciendo toda la info
              Connection conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/chatbot", "chatbot", "chatbot") ;
+             // En la conexión creo una petición
              Statement stmt =  conn.createStatement() ;
-             ResultSet rs;
-
-             rs = stmt.executeQuery("SELECT * FROM webchat_lines LIMIT 1");
-             while ( rs.next() ) {
+             // Contenedor de información correspondiente a un array que contendrá el resultado de la petición
+             ResultSet rs;          
+             // Quiero coger lo que hay dentro del textfield para usarlo en la peticion SQL
+             String contienetext = entrada.getText();
+             // Ese contenedor, ya no esta vacío, es igual al resultado de la siguiente peticion
+             rs = stmt.executeQuery(""
+                     + "SELECT * FROM webchat_lines"
+                     + " WHERE text LIKE '%"+contienetext+"%'"
+                     + " LIMIT 1"
+                     + "");
+             int idseleccionado = 0;
+             while ( rs.next() ) {  // Mientras que RS siga teniendo lineas (rows)
+                 idseleccionado = rs.getInt("id");
+             }
+             rs = stmt.executeQuery(""
+                     + "SELECT * FROM webchat_lines"
+                     + " WHERE id = "+(idseleccionado+1)+""
+                     + ""
+                     + "");
+             while ( rs.next() ) {  // Mientras que RS siga teniendo lineas (rows)
+                 // Avanza el puntero a la siguiente linea de resultado
+                 // Mensaje es dame la string que hay dentro de la columna text
                  String mensaje = rs.getString("text");
+                 // Escribeme el contenido
                  salida.setText(mensaje);
              }
+             // Cierrame la conexión
              conn.close();
          } catch (Exception e) {
              System.err.println("Got an exception! ");
