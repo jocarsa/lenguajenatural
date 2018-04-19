3785d94b40b5ceb1814b991183b7c5bafbc2abd7
jocarsa <info@josevicentecarratala.com>
2018-04-19 19:32:48 +0200

    Poniendo Swing

--- lenguajenatural/src/lenguajenatural/Lenguajenatural.java
+++ lenguajenatural/src/lenguajenatural/Lenguajenatural.java
@@ -5,17 +5,45 @@
  */
 package lenguajenatural;
 
-/**
- *
- * @author A1P9
- */
-public class Lenguajenatural {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+    
+
+public class Lenguajenatural extends JFrame {
+        private JLabel rotulo;
+        private JTextField entrada;
+        private JLabel salida;
+        private JButton boton;
+    
+     public Lenguajenatural(){
+         rotulo = new JLabel("Chatbot Raulero");
+         rotulo.setBounds(30,50,150,20);
+        add(rotulo);
+        
+        entrada = new JTextField();
+        entrada.setBounds(30,80,150,20);
+        add(entrada);
+        
+        boton = new JButton("Envia");
+        boton.setBounds(30,100,150,20);
+        add(boton);
+        
+        salida = new JLabel("salida");
+         salida.setBounds(30,150,150,20);
+        add(salida);
+     }
+     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         Lenguajenatural ventana = new Lenguajenatural();
+        ventana.setBounds(50, 50, 300, 400);
+        ventana.setVisible(true);
     }
     
 }
